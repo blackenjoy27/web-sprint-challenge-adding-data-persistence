811214462f6432bb1c80081b8aa7ce95714c3ff3
blackenjoy27 <lixunxin@gmail.com>
2021-06-11 13:49:43 -0400

    wip

--- api/project/model.js
+++ api/project/model.js
@@ -2,23 +2,26 @@ const db = require("../../data/dbConfig");
 
 const getAll = async () => {
     const data = await db("projects");
-    return data.map(project=>{
-        return {...project, project_completed: 
-            Boolean(project.project_completed)}
+    return data.map(project => {
+        return {
+            ...project, project_completed:
+                Boolean(project.project_completed)
+        }
     });
 }
 
 const getById = async (id) => {
     const data = await db("projects").
-    where("project_id",id).first();
+        where("project_id", id).first();
 
     return {
-        ...data, 
-        project_completed: 
-        Boolean(data.project_completed)}
+        ...data,
+        project_completed:
+            Boolean(data.project_completed)
+    }
 }
 
-const addNewProject = async (project) =>{
+const addNewProject = async (project) => {
     const id = await db("projects").insert(project);
     return getById(id);
 }
--- api/project/router.js
+++ api/project/router.js
@@ -2,20 +2,20 @@ const express = require("express");
 const projects = require("./model");
 const router = express.Router();
 
-router.get("/",(req,res,next)=>{
+router.get("/", (req, res, next) => {
     projects.getAll()
-    .then(allProjects =>{
-        res.status(200).json(allProjects)
-    })
-    .catch(next);
+        .then(allProjects => {
+            res.status(200).json(allProjects)
+        })
+        .catch(next);
 })
 
-router.post("/",(req,res,next)=>{
+router.post("/", (req, res, next) => {
     projects.addNewProject(req.body)
-    .then(newProject=>{
-        res.status(201).json(newProject)
-    })
-    .catch(next);
+        .then(newProject => {
+            res.status(201).json(newProject)
+        })
+        .catch(next);
 })
 
 
