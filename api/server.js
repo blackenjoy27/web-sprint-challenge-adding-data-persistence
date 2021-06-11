b52c258348ab0b10d3b42fa3979e9dcdb10a7ee4
blackenjoy27 <lixunxin@gmail.com>
2021-06-11 13:30:10 -0400

    Finish the resource router tests

--- api/server.js
+++ api/server.js
@@ -1,6 +1,17 @@
 const express = require("express");
 
 const server = express();
+const resourceRouter = require("./resource/router");
+// const resourceRouter = require("./resource/router");
+// const projectRouter = require("./resource/router");
+// const taskRouter = require("./resource/router");
+
+server.use(express.json());
+
+server.use("/api/resources",resourceRouter);
+// server.use("/api/projects",projectRouter);
+// server.use("/api/tasks",taskRouter);
+
 
 server.use((error,req,res,next)=>{
     res.status(error.status||500).json({
